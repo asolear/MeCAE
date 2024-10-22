06a13c278d6f8aa830edbfcf34ccc44fa5e91a7a
Your Name <you@example.com>
2024-10-22 20:59:35 +0200

    rr mar 22 oct 2024 20:59:35 CEST

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -14,16 +14,27 @@ import { getFirestore } from "firebase/firestore"
 //     measurementId: "G-W9NBWXRCCF"
 //   };
   
-  
-  const firebaseConfig = {
-    apiKey: "AIzaSyDzbra3gwYs5M3YthoMIuofG2QxCAdkmzc",
-    authDomain: "mecae-11751.firebaseapp.com",
-    projectId: "mecae-11751",
-    storageBucket: "mecae-11751.appspot.com",
-    messagingSenderId: "213099887832",
-    appId: "1:213099887832:web:8a3ff62d35b844c184abad",
-    measurementId: "G-RPDDN9R888"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDY1GhUR47WGpJcNhpq2gPTOKIyCJ1_oaw",
+  authDomain: "prueba-440b0.firebaseapp.com",
+  databaseURL: "https://prueba-440b0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "prueba-440b0",
+  storageBucket: "prueba-440b0.appspot.com",
+  messagingSenderId: "799443941595",
+  appId: "1:799443941595:web:6d12fa146ec1308a20f4fe",
+  measurementId: "G-W9NBWXRCCF"
+};  
+
+
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyDzbra3gwYs5M3YthoMIuofG2QxCAdkmzc",
+  //   authDomain: "mecae-11751.firebaseapp.com",
+  //   projectId: "mecae-11751",
+  //   storageBucket: "mecae-11751.appspot.com",
+  //   messagingSenderId: "213099887832",
+  //   appId: "1:213099887832:web:8a3ff62d35b844c184abad",
+  //   measurementId: "G-RPDDN9R888"
+  // };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
